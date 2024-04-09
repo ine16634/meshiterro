225b3d5aa4dfba0ab8c96b3a144f9a673d1c032e
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-04-09 12:32:34 +0000

    [Finish] 1-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_08_101854) do
+ActiveRecord::Schema.define(version: 2024_04_09_115520) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.string "body"
+    t.text "caption"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
