26ade0e0e4f1866aee7866dfcbe0a2c87b1a7062
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-04-14 22:06:43 +0000

    [Finish] 1-13

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -10,12 +10,19 @@ class PostImagesController < ApplicationController
     @post_image.save
     redirect_to post_images_path
   end
+  
+  def destroy
+    post_image = PostImage.find(params[:id]) 
+    post_image.destroy
+    redirect_to '/post_images'
+  end
 
   def index
     @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id]) 
   end
   
     # 投稿データのストロングパラメータ
