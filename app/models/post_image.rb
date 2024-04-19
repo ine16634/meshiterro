69206c7b2306abe9c60c8fc4213461d7973318eb
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-04-19 13:12:44 +0000

    [Finish] 1-22+

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,16 @@ class PostImage < ApplicationRecord
   
    has_one_attached :image
    belongs_to :user
+   has_many :post_comments, dependent: :destroy
+   has_many :favorites, dependent: :destroy
    
+   validates :shop_name, presence: true
+   validates :image, presence: true
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end 
+  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -10,5 +19,7 @@ class PostImage < ApplicationRecord
     end
     image
   end
+   
+   
    
 end
