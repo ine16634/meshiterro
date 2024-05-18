37b6f9d656af7571f466c5b855badec086bc986d
ine16634 <62016478+ine16634@users.noreply.github.com>
2024-05-18 06:20:21 +0000

    [Finish]管理者機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,10 @@ Rails.application.routes.draw do
   
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
+     resources :users, only: [:destroy]
   end
 
-
+ scope module: :public do
     devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
@@ -20,4 +21,5 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:show, :edit, :update]
    
+  end
 end
